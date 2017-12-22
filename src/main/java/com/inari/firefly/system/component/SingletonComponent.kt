3d4485f79e12aae7f8fe823525b478cbf4bbd983
andreas hefti <anhefti@yahoo.de>
2017-12-22 14:59:01 +0100

    API singleton component

--- src/main/java/com/inari/firefly/system/component/SingletonComponent.kt
+++ src/main/java/com/inari/firefly/system/component/SingletonComponent.kt
@@ -1,11 +1,20 @@
 package com.inari.firefly.system.component
 
 import com.inari.firefly.FFContext
+import com.inari.firefly.NO_COMP_ID
+import com.inari.firefly.component.CompId
 import com.inari.firefly.component.ComponentType
 
 abstract class SingletonComponent<CC : C, C : SystemComponent> : ComponentType<C> {
     abstract fun subType(): Class<CC>
     protected abstract fun create(): CC
+
+    val id: CompId
+        get() {
+            return if (FFContext.mapper(this).contains(subType().simpleName))
+                 FFContext[this, subType().simpleName].componentId
+            else NO_COMP_ID
+        }
 
     operator fun invoke(): CC {
         if (!FFContext.mapper(this).contains(subType().simpleName)) {
