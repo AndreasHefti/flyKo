d15f87437961203714eb039202629c036299a0e2
andreas hefti <anhefti@yahoo.de>
2018-08-22 21:52:47 +0200

    fix

--- src/main/java/com/inari/firefly/graphics/particle/EParticle.kt
+++ src/main/java/com/inari/firefly/graphics/particle/EParticle.kt
@@ -5,14 +5,15 @@ import com.inari.firefly.component.ComponentRefResolver
 import com.inari.firefly.entity.EntityComponent
 import com.inari.firefly.entity.EntityComponentType
 import com.inari.firefly.graphics.rendering.Renderer
+import com.inari.firefly.graphics.rendering.SpriteParticleRenderer
 import com.inari.util.collection.DynArray
 
 class EParticle private constructor() : EntityComponent() {
 
-    @JvmField internal var rendererRef = -1
+    @JvmField internal var rendererRef = SpriteParticleRenderer.aspectIndex
     private val particle: DynArray<Particle> = DynArray.of(Particle::class.java)
 
-    var ff_Renderer = ComponentRefResolver(Renderer) { index-> rendererRef =index }
+    var ff_Renderer = ComponentRefResolver(Renderer) { index-> rendererRef = index }
     var ff_Particle = ArrayAccessor(particle)
 
     @Suppress("UNCHECKED_CAST")
